0a7735beeb1d37f6fc0596eec72e7a03227dabfe
Andrey Tonkikh <xosmig@gmail.com>
2016-10-07 02:32:24 +0300

    not operator added

--- src/main.py
+++ src/main.py
@@ -50,6 +50,8 @@ class Src:
     # ~ over a text. Usage ~^R , ~^{text}
     tilde_over = "~\^"
     overline = "\-\^"
+    # not operator. examples: != !\in
+    not_op = "!"
 
 
 class Tar:
@@ -78,8 +80,9 @@ def main():
         # warning: <=> should be parsed before => and <=
         Operation(OpType.replace, "<=>", r"\\Leftrightarrow"),
         Operation(OpType.replace, "=>", r"\\Rightarrow"),
-        # two kinds of not_equal operators
-        Operation(OpType.replace, "!=", r"\\neq"),
+        # not operator. examples: != !\in
+        Operation(OpType.replace, Src.not_op, r"\\not"),
+        # not_equal operator
         Operation(OpType.replace, "/=", r"\\neq"),
         # comparison operators
         Operation(OpType.replace, "<=", r"\\le"),
