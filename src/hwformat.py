fc7ad69f94639c7bd8a9fe912816be9ad2e22f23
Andrey Tonkikh <xosmig@gmail.com>
2016-10-07 17:25:56 +0300

    local operations --> global OPERATIONS

--- src/hwformat.py
+++ src/hwformat.py
@@ -8,6 +8,7 @@
 # TODO: division (common, nested)
 # TODO: help message
 # TODO: complex example
+# TODO: good default header
 
 import re
 import sys
@@ -17,64 +18,64 @@ from operations import OpType, Operation
 
 FORMAT = "hw"
 
+OPERATIONS = [
+    # trim extra spaces in the end of line
+    Operation(OpType.replace, r"[ ]+$", ""),
+    # comments.
+    Operation(OpType.replace, patterns.LINE_COMMENT, target.LINE_COMMENT),
+]
+
+OPERATIONS_MATH = [
+    # wrap russian in text block:
+    Operation(OpType.replace, "( ?[а-яА-ЯёЁ]+ ?)", r"\\text{\1}\\allowbreak"),
+    # warning: <=> should be parsed before => and <=
+    Operation(OpType.replace, "<=>", r"\\Leftrightarrow"),
+    Operation(OpType.replace, "=>", r"\\Rightarrow"),
+    # two kinds of not_equal operators
+    Operation(OpType.replace, "!=", r"\\neq"),
+    Operation(OpType.replace, "/=", r"\\neq"),
+    # comparison operators
+    Operation(OpType.replace, "<=", r"\\le"),
+    Operation(OpType.replace, ">=", r"\\ge"),
+    # arrows
+    Operation(OpType.replace, "\->", r"\\rightarrow"),
+    Operation(OpType.replace, "<\-", r"\\leftarrow"),
+    # equals sign with three lines
+    Operation(OpType.replace, "==", r"\\equiv"),
+    # equals symbol with ~ (isomorphism)
+    Operation(OpType.replace, patterns.ISOMORPHIC, "\\cong"),
+    # ~ over a text
+    Operation(OpType.replace, patterns.TILDE_OVER, r"\\widetilde "),
+    # line over a text
+    Operation(OpType.replace, patterns.OVERLINE, r"\\overline "),
+    # plus-minus symbol
+    Operation(OpType.replace, r"\+\-", r"\\pm"),
+    # not-in operator
+    Operation(OpType.replace, patterns.NOT_IN, r"\\not\\in"),
+    # TODO: division
+    # -e 's/$(LEFT)\/$(RIGHT)/\\frac{\1}{\2}/g' \
+    # FIXME: can be used only in math mode:
+    Operation(OpType.replace, patterns.COMMENT_OPEN, target.MATH_CLOSE + target.COMMENT_OPEN),
+    Operation(OpType.replace, patterns.COMMENT_CLOSE, target.COMMENT_CLOSE + target.MATH_OPEN),
+    # multiply
+    Operation(OpType.replace, r"\*", r"\\cdot"),
+    # non-math block inline:
+    Operation(OpType.replace, patterns.NON_MATH_OPEN, target.MATH_CLOSE),
+    Operation(OpType.replace, patterns.NON_MATH_CLOSE, target.MATH_OPEN),
+]
+
 
 def main():
     if len(sys.argv) != 2:
         print("TODO: Help message")
         exit(1)
 
-    operations = [
-        # trim extra spaces in the end of line
-        Operation(OpType.replace, r"[ ]+$", ""),
-        # comments.
-        Operation(OpType.replace, patterns.LINE_COMMENT, target.LINE_COMMENT),
-    ]
-
-    operations_math = [
-        # wrap russian in text block:
-        Operation(OpType.replace, "( ?[а-яА-ЯёЁ]+ ?)", r"\\text{\1}\\allowbreak"),
-        # warning: <=> should be parsed before => and <=
-        Operation(OpType.replace, "<=>", r"\\Leftrightarrow"),
-        Operation(OpType.replace, "=>", r"\\Rightarrow"),
-        # two kinds of not_equal operators
-        Operation(OpType.replace, "!=", r"\\neq"),
-        Operation(OpType.replace, "/=", r"\\neq"),
-        # comparison operators
-        Operation(OpType.replace, "<=", r"\\le"),
-        Operation(OpType.replace, ">=", r"\\ge"),
-        # arrows
-        Operation(OpType.replace, "\->", r"\\rightarrow"),
-        Operation(OpType.replace, "<\-", r"\\leftarrow"),
-        # equals sign with three lines
-        Operation(OpType.replace, "==", r"\\equiv"),
-        # equals symbol with ~ (isomorphism)
-        Operation(OpType.replace, patterns.ISOMORPHIC, "\\cong"),
-        # ~ over a text
-        Operation(OpType.replace, patterns.TILDE_OVER, r"\\widetilde "),
-        # line over a text
-        Operation(OpType.replace, patterns.OVERLINE, r"\\overline "),
-        # plus-minus symbol
-        Operation(OpType.replace, r"\+\-", r"\\pm"),
-        # not-in operator
-        Operation(OpType.replace, patterns.NOT_IN, r"\\not\\in"),
-        # TODO: division
-        # -e 's/$(LEFT)\/$(RIGHT)/\\frac{\1}{\2}/g' \
-        # FIXME: can be used only in math mode:
-        Operation(OpType.replace, patterns.COMMENT_OPEN, target.MATH_CLOSE + target.COMMENT_OPEN),
-        Operation(OpType.replace, patterns.COMMENT_CLOSE, target.COMMENT_CLOSE + target.MATH_OPEN),
-        # multiply
-        Operation(OpType.replace, r"\*", r"\\cdot"),
-        # non-math block inline:
-        Operation(OpType.replace, patterns.NON_MATH_OPEN, target.MATH_CLOSE),
-        Operation(OpType.replace, patterns.NON_MATH_CLOSE, target.MATH_OPEN),
-    ]
-
     file_path = sys.argv[1]
 
     with open(file_path, "r") as source:
         with open(re.sub(r"\." + FORMAT, r".tex", file_path), "w") as dest:
             for line in source.readlines():
-                for op in operations:
+                for op in OPERATIONS:
                     line = op.do(line)
 
                 if line.startswith(patterns.RAW):
@@ -82,7 +83,7 @@ def main():
                 elif line != "\n" and not line.startswith(target.LINE_COMMENT):
                     # if line isn't empty nor comment:
 
-                    for op in operations_math:
+                    for op in OPERATIONS_MATH:
                         line = op.do(line)
 
                     # TODO: header support
